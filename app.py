e1bdfc89fe922b8b6c411e526371cfcca4468c6a
Caleb Barnard <poisonpsychicghost@gmail.com>
2026-06-25 10:57:01 -0400

    Day Two: Attached a dummy index.html

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-from flask import Flask
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
 print("App file Loaded")
+
+@app.route("/")
+def home():
+    return send_file("index.html")
+
+@app.route("/about")
+def about():
+    print("The /about route was requested")
+    return "<h1>This API will manage movie data.</h1>"
+
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
