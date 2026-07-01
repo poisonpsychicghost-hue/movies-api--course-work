269d3425b9d12d3e0fa99294937d9969c562fd6f
Caleb Barnard <poisonpsychicghost@gmail.com>
2026-07-01 09:38:06 -0400

    Added Dummy Movie Array Lesson

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, jsonify
 
 app = Flask(__name__)
 
@@ -13,7 +13,21 @@ def about():
     print("The /about route was requested")
     return "<h1>This API will manage movie data.</h1>"
 
+@app.route("/movie")
+def get_movie():
+    print('Movie Route Selected!')
+    base_title = "Dazed and Confused"
+    year = 1993
+    full_title = f"{base_title} ({year})"
+    director = "Richard Linklater"
 
+    movie = {
+        "id": 1,
+        "title": full_title,
+        "year": year,
+        "director": director
+    }
+    return jsonify(movie)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
