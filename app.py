6a1f3e7c6045ede4922042ec4984205458e1ab0b
Caleb Barnard <poisonpsychicghost@gmail.com>
2026-07-01 09:38:17 -0400

    Lesson

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
-from flask import Flask, send_file, jsonify
+from flask import Flask, send_file, jsonify, request
 
 app = Flask(__name__)
 
 print("App file Loaded")
+
+movies = [
+    {
+        "id": 0,
+        "title": "Dazed and Confused",
+        "director": "Richard Linklater",
+        "year": 1993,
+        "watched": True
+    },
+    {
+        "id": 1,
+        "title": "Spirited Away",
+        "director": "Hayao Miyazaki",
+        "year": 2001,
+        "watched": True
+    }
+]
+print("Movies at Startup", movies)
 
 @app.route("/")
 def home():
@@ -13,21 +31,17 @@ def about():
     print("The /about route was requested")
     return "<h1>This API will manage movie data.</h1>"
 
-@app.route("/movie")
-def get_movie():
-    print('Movie Route Selected!')
-    base_title = "Dazed and Confused"
-    year = 1993
-    full_title = f"{base_title} ({year})"
-    director = "Richard Linklater"
+@app.route("/movies")
+def get_movies():
+    print('Movies Route Selected!')
 
-    movie = {
-        "id": 1,
-        "title": full_title,
-        "year": year,
-        "director": director
-    }
-    return jsonify(movie)
+    director = request.args.get("director")
+    
+    if director is None: 
+        return jsonify(movies)
+    
+    filtered = [m for m in movies if m["director"] == director]
+    return jsonify(filtered)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
